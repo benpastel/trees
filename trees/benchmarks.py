39ce8afc3bad910992d3e55f42d69e6832c10fb8
benji <benpastel@gmail.com>
2020-03-19 13:44:20 -0700

    m5 timeseries benchmark

--- trees/benchmarks.py
+++ trees/benchmarks.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import xgboost as xgb
@@ -80,26 +81,109 @@ def load_santander():
   return X, y
 
 
+M5_CACHE = 'data/m5/cache.npz'
+def load_m5():
+  if os.path.isfile(M5_CACHE):
+    print('loading from cache')
+    cached = np.load(M5_CACHE)
+    return cached['X'], cached['y']
+
+  with timed('loading data from csv...'):
+    frame = pd.read_csv('data/m5/sales_train_validation.csv')
+
+    # input format:
+    #   one row per item, one column per day
+    items = 30490
+    days = 1912
+    sales = np.zeros((items, days), dtype=int)
+    for d in range(days):
+      sales[:, d] = frame[f'd_{d+1}'].values
+
+    y = sales[:, 365:].flatten()
+
+    # start with a very simple timeseries prediction setup
+    # 
+    # skip the first 365 days, then predict each day based on:
+    #   the previous day
+    #   the previous 7 days
+    #   the previous 28 days
+    #   the previous 365 days
+    target_days = days - 365
+    rows = items * target_days
+
+    # this axes order is efficient for writing
+    # approximate with uint16 for efficiency
+    # scale floats before the uint18 truncation:
+    # 
+    #   np.max(sales) == 763 so 80 is a safe scale for mean
+    #   np.std is harder to predict, so use 10 to be more cautious
+    feats = 14
+    X = np.zeros((target_days, feats, items), dtype=np.uint16)
+    sales_T = sales.T
+    for t in range(target_days):
+      if (t % 100) == 0:
+        print(f'{t}/{target_days}')
+      d = t + 365
+
+      # 1 feature: previous day
+      X[t, 0] = sales_T[d-1]
+
+      # 5 features: previous 7 days
+      X[t, 1] = sales_T[d-7]
+      X[t, 2] = np.mean(sales_T[d-7:d], axis=0) * 80
+      X[t, 3] = np.min(sales_T[d-7:d], axis=0)
+      X[t, 4] = np.max(sales_T[d-7:d], axis=0)
+      X[t, 5] = np.std(sales_T[d-7:d], axis=0) * 10
+
+      # 4 features: previous 28 days
+      # (min is almost always going to be 0)
+      X[t, 6] = sales_T[d-28]
+      X[t, 7] = np.mean(sales_T[d-28:d], axis=0) * 80
+      X[t, 8] = np.max(sales_T[d-28:d], axis=0)
+      X[t, 9] = np.std(sales_T[d-28:d], axis=0) * 10
+
+      # 5 features: previous 365 days
+      # (min is almost always going to be 0)
+      X[t, 10] = sales_T[d-365]
+      X[t, 11] = np.mean(sales_T[d-365:d], axis=0) * 80
+      X[t, 12] = np.max(sales_T[d-365:d], axis=0)
+      X[t, 13] = np.std(sales_T[d-365:d], axis=0) * 10
+
+    # (target_days, feats, items) => (items, target_days, feats)
+    X = np.moveaxis(X, -1, 0)
+
+    # (items, target_days, feats) => (rows, feats)
+    X = X.reshape((items * target_days, feats))
+
+  with timed('saving...'):
+    np.savez_compressed(M5_CACHE, X=X, y=y)
+  return X, y
+
+
 if __name__ == '__main__':
   benchmark_names = [
     'Agaricus', 
     'Home Credit Default Risk', 
     'Santander Value', 
+    'M5'
   ]
 
   load_data_functions = [
     load_agaricus,
     load_credit,
-    load_santander
+    load_santander,
+    load_m5
   ]
 
   xgboost_args = [
     {'n_estimators': 1, 'tree_method': 'exact'},
     {'n_estimators': 1, 'tree_method': 'hist'},
     {'n_estimators': 1, 'tree_method': 'hist'},
+    {'n_estimators': 1, 'tree_method': 'hist'},
   ]
 
   tree_args = [
+    {'min_leaf_size': 10, 'max_depth': 6, 'extra_leaf_penalty': 0.0},
     {'min_leaf_size': 10, 'max_depth': 6, 'extra_leaf_penalty': 0.0},
     {'min_leaf_size': 10, 'max_depth': 6, 'extra_leaf_penalty': 0.0},
     {'min_leaf_size': 10, 'max_depth': 6, 'extra_leaf_penalty': 0.0},
