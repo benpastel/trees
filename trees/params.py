3d8dc3057e63db32646aac244306d2ecfb1f4f1b
benji <benpastel@gmail.com>
2020-04-15 16:44:48 -0700

    update log and params

--- trees/params.py
+++ trees/params.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Params:
-  smooth_factor: float = 10.0
+  smooth_factor: float = 100.0
   max_nodes: int = 3280
-  max_depth: int = 7
+  max_depth: int = 8
   tree_count: int = 10
   learning_rate: float = 0.3
 
