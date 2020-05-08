e3b6742a49c179aa77bc1868c30dad854aa646da
benji <benpastel@gmail.com>
2020-05-08 16:18:00 -0700

    make do with bucket_count=64 and update log

--- trees/params.py
+++ trees/params.py
@@ -9,7 +9,7 @@ class Params:
   tree_count: int = 10
   learning_rate: float = 0.3
   third_split_penalty: float = 4.0
-  bucket_count: int = 128
+  bucket_count: int = 64
   bucket_sample_count: int = 10000
   trees_per_bucketing: int = 1
 
